c6eaa67f3410cd5ce39879f51d2b9e86ac097e3f
Selwyn <talk@selwyn.cc>
2018-08-08 17:44:50 +0200

    Switch to JSX

--- exercise-3/apollo-solution.js
+++ exercise-3/apollo-solution.js
@@ -2,7 +2,7 @@
 
 const ApolloClient = require('apollo-boost').default;
 const gql = require('graphql-tag');
-const { createElement: element } = require('react');
+const { createElement } = require('react');
 const { ApolloProvider, Query } = require('react-apollo');
 const { render } = require('react-dom');
 
@@ -13,42 +13,42 @@ const client = new ApolloClient({
   },
 });
 
-const voorhoedeMembers = ({ organization }) =>
-  organization.members.nodes.map(member =>
-    element('figure', { key: member.id }, [
-      element('img', { src: member.avatarUrl, key: 'image' }),
-      element('figcaption', { key: 'caption' }, member.name || member.login),
-    ])
-  );
+const voorhoedeMembers = ({ organization }) => (
+  organization.members.nodes.map(member => (
+    <figure key={member.id}>
+      <img src={member.avatarUrl} />
+      <figcaption>{member.name || member.login}</figcaption>
+    </figure>
+  ))
+);
 
-const fetchVoorhoedeMembers = () =>
-  element(
-    Query,
-    {
-      query: gql`
-        {
-          organization(login: "voorhoede") {
-            members(first: 100) {
-              nodes {
-                id
-                name
-                login
-                avatarUrl(size: 200)
-              }
+const fetchVoorhoedeMembers = () => (
+  <Query
+    query={gql`
+      {
+        organization(login: "voorhoede") {
+          members(first: 100) {
+            nodes {
+              id
+              name
+              login
+              avatarUrl(size: 200)
             }
           }
         }
-      `,
-    },
-    ({ loading, error, data }) => {
-      if (loading) return element('marquee', null, 'Loading...');
-      if (error) return element('p', null, `Error ${error}`);
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <marquee>Loading...</marquee>;
+      if (error) return <p>Error {error}</p>;
 
       return voorhoedeMembers({ organization: data.organization });
-    }
-  );
+    }}
+  </Query>
+);
 
 render(
-  element(ApolloProvider, { client }, fetchVoorhoedeMembers()),
+  createElement(ApolloProvider, { client }, fetchVoorhoedeMembers()),
   document.querySelector('[data-react-container]')
 );
--- exercise-3/webpack.config.js
+++ exercise-3/webpack.config.js
@@ -2,6 +2,21 @@ const dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   entry: './exercise-3/apollo-exercise.js',
   plugins: [new dotenv({ safe: true })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [['transform-react-jsx', { pragma: 'createElement' }]],
+          },
+        },
+      },
+    ],
+  },
 };
